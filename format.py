767d38b1ac71d73a8a1eb3cbcf6d3ac25bf370af
Kevin <kevin@Kevins-MacBook-Pro.local>
2020-07-23 22:41:57 -0400

    added data format

---
+++ format.py
@@ -0,0 +1,22 @@
+
+class Row:
+
+    def __init__(self, province, lv_of_gov, organization, riding_name, about_district,\
+    about_council, election_info, primary_role, other_role, bio, name, first_name, last_name,\
+    gender, party_name, email, photo_url, source_url, website, facebook, instagram, twitter, linkedin,\
+    youtube, office_type, address, phone, fax):
+        self.province = province
+        self.lv_of_gov = lv_of_gov
+        self.organization = organization
+        self.riding_name = riding_name
+        self.about_district = about_district
+        self.about_council = about_district
+        self.election_info = election_info
+        self.primary_role = primary_role
+        self.other_role = other_role
+        self.bio = bio
+        self.name = name
+        self.first_name = first_name
+        self.last_name = last_name
+        self.gender = gender
+        self.party_name = party_name
